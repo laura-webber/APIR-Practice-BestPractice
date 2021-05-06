f80a7ece69915e7650db9c229cc94f55ce1a6f96
Craig Neff <ifly12610@gmail.com>
2021-05-06 00:42:58 -0600

    still wrapping my head around this still a bit fuzzy but I feel better ending tonight than I did last night. Getting better everyday

---
+++ src/Components/HousesList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { House } from "./House";
+import { housesApi } from "../rest/HousesApi.js";
+
+export class HouseList extends React.Component {
+    state = {
+        houses = []
+    };
+
+    componentDidMount() {
+        this.fetchHouses();
+    };
+
+    fetchHouses = async () => {
+        const houses = await housesApi.get();
+        this.setState({ houses });
+    };
+
+    updateHouse = async (updateHouse) => {
+        await housesApi.put(updateHouse);
+        this.fetchHouses();
+    };
+
+    render() {
+        return (
+            <div className="house-list">
+                {this.state.houses.map((house) => (
+                    <House 
+                        house={house}
+                        key={house._id}
+                        updateHouse={this.updateHouse}
+                        />
+                ))}
+
+            </div>
+        )
+    }
+    
+
+}
