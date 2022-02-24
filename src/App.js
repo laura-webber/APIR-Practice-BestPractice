9844fe0394933f078669500eade3c900e7d17d62
Laura Webber <laura@hightouch.io>
2022-02-24 13:09:24 -0800

    update
    update

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 class App extends Component {
   render() {
     return (
+      // eslint-disable-next-line react/jsx-no-comment-textnodes
       <div className="craig">
         <img
           src="https://blog.realestate.cornell.edu/files/2018/10/Northville-Crossing-rvb09e.jpg"
           className="img-fluid"
-          alt="Responsive image"
           width="500"
           height="600"
+          alt=""
         />
         <h4> A Community Builder For Tomorrow</h4>
         <Navbar />
--- src/Components/AddHouse.js
+++ src/Components/AddHouse.js
@@ -2,22 +2,21 @@ import React, { Component } from "react";
 
 export class AddHouse extends Component {
   constructor(props) {
-    supe(props);
-    state = {
+    super(props);
+    this.state = {
       houseName: "",
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.setState([hous]);
+    this.setState(console.log("setState triggered"));
   }
   render() {
     return (
       <div>
         <h1>{this.state.count}</h1>
         <button onClick={this.handleClick}>Change!</button>
-        <ChildComponent count={this.state.count} />
       </div>
     );
   }
